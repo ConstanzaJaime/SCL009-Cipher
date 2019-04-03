31130e9ebd862934cb20c1a4cbe560af52e46ad6
ConstanzaJaime <constanza.jaime.a@gmail.com>
2019-04-03 15:58:08 -0300

    actualizacion html y cifrado javascript

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 document.getElementById("encodeButton").addEventListener("click", () => {
-    let string= document.getElementById("inputMessage").value.toUpperCase();
+    let preString= document.getElementById("inputMessage").value ;
+    let string= preString.toUpperCase();
     let offset= parseInt(document.getElementById("offset").value);
     let inputText="";
     
     for(let i=0;i< string.length;i++){
     
-      let valorASCII=string[i].charCodeAt(0);
-    
+      let valorASCII=string[i].charCodeAt(0);//da valor ASCII 
+          console.log(valorASCII);
       if(valorASCII>=65 && valorASCII<=90){ 
       let valorASCIIoffset=(valorASCII-65+offset)%26+65;//imprime código de ASCII + offset
-      letraCodificada= String.fromCharCode(valorASCIIoffset)//Imprime letra codificada; 
-          console.log(letraCodificada)
-       
+          console.log(valorASCIIoffset);
+      letraCodificada= String.fromCharCode(valorASCIIoffset);//Imprime letra codificada; 
+          console.log(letraCodificada);
+          
+          
     }
       inputText= inputText + letraCodificada;
     
@@ -19,3 +22,29 @@ document.getElementById("encodeButton").addEventListener("click", () => {
     
     document.getElementById("outputMessage").value= inputText;
     });
+
+
+document.getElementById("decodeButton").addEventListener("click", () => {
+      let preString= document.getElementById("inputMessage").value ;
+      let string= preString.toUpperCase();
+      let offset= parseInt(document.getElementById("offset").value);
+      let inputText="";
+      
+      for(let i=0;i< string.length;i++){
+      
+        let valorASCII=string[i].charCodeAt(0);
+            console.log(valorASCII);
+        if(valorASCII>=65 && valorASCII<=90){ 
+        let valorASCIIoffset=(valorASCII-65-offset)%26+65;//imprime código de ASCII + offset
+            console.log(valorASCIIoffset);
+        letraCodificada= String.fromCharCode(valorASCIIoffset);//Imprime letra codificada; 
+            console.log(letraCodificada);
+            
+            
+      }
+        inputText= inputText + letraCodificada;
+      
+      } 
+      
+      document.getElementById("outputMessage").value= inputText;
+      });
