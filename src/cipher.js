0166a89ebdb0cc9b492295c1fa95f93e458c2caa
ConstanzaJaime <constanza.jaime.a@gmail.com>
2019-04-04 17:04:28 -0300

    Cifrado y decifrado + botones en DOM

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,33 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (string,offset) => { ////FUNCION CODIFICAR
+    let outputText="";
     
+    for(let i=0;i< string.length;i++){ //Ciclo de codificado
+      
+      let originalASCII=string[i].charCodeAt(0); //Código ASCII de caracteres ingresados
+
+      if(originalASCII>=65 && originalASCII<=90){ //Rango de caracteres en mayúsculas según código ASCII
+      let offsetASCII=(originalASCII-65+offset)%26+65; //Código ASCII de caracteres de salida
+      encodeText= String.fromCharCode(offsetASCII);//Caracteres codificados    
+    }
+      outputText += encodeText; //Concatenación de caracteres codificados
+    } 
+    return outputText //Concatenación de caracteres codificados
   },
-  decode: () => {
-    /* Acá va tu código */
+
+  decode: (string,offset) => {  ////FUNCION DECODIFICAR
+    let outputText="";
+      
+    for(let i=0;i< string.length;i++){ //Ciclo de decodificado
+      
+      let originalASCII=string[i].charCodeAt(0); //Código ASCII de caracteres ingresados
+
+      if(originalASCII>=65 && originalASCII<=90){   //Rango de caracteres en mayúsculas según código ASCII
+      let offsetASCII=(originalASCII+65-offset)%26+65;  //Código ASCII de caracteres de salida
+      decodeText= String.fromCharCode(offsetASCII);   //Caracteres decodificados  
+    }
+      outputText += decodeText;  //Concatenación de caracteres decodificados
+    } 
+    return outputText //Concatenación de caracteres decodificados
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,50 +1,27 @@
+///BOTON CODIFICAR
 document.getElementById("encodeButton").addEventListener("click", () => {
-    let preString= document.getElementById("inputMessage").value ;
-    let string= preString.toUpperCase();
-    let offset= parseInt(document.getElementById("offset").value);
-    let inputText="";
-    
-    for(let i=0;i< string.length;i++){
-    
-      let valorASCII=string[i].charCodeAt(0);//da valor ASCII 
-          console.log(valorASCII);
-      if(valorASCII>=65 && valorASCII<=90){ 
-      let valorASCIIoffset=(valorASCII-65+offset)%26+65;//imprime código de ASCII + offset
-          console.log(valorASCIIoffset);
-      letraCodificada= String.fromCharCode(valorASCIIoffset);//Imprime letra codificada; 
-          console.log(letraCodificada);
-          
-          
-    }
-      inputText= inputText + letraCodificada;
-    
-    } 
-    
-    document.getElementById("outputMessage").value= inputText;
-    });
 
+  let string= document.getElementById("inputMessage").value.toUpperCase() ;
+  let offset= parseInt(document.getElementById("offset").value);
+  let encodeText=cipher.encode(string,offset);
 
+    document.getElementById("outputMessage").value= encodeText;
+});
+
+///BOTON DECODIFICAR
 document.getElementById("decodeButton").addEventListener("click", () => {
-      let preString= document.getElementById("inputMessage").value ;
-      let string= preString.toUpperCase();
-      let offset= parseInt(document.getElementById("offset").value);
-      let inputText="";
-      
-      for(let i=0;i< string.length;i++){
-      
-        let valorASCII=string[i].charCodeAt(0);
-            console.log(valorASCII);
-        if(valorASCII>=65 && valorASCII<=90){ 
-        let valorASCIIoffset=(valorASCII-65-offset)%26+65;//imprime código de ASCII + offset
-            console.log(valorASCIIoffset);
-        letraCodificada= String.fromCharCode(valorASCIIoffset);//Imprime letra codificada; 
-            console.log(letraCodificada);
-            
-            
-      }
-        inputText= inputText + letraCodificada;
-      
-      } 
-      
-      document.getElementById("outputMessage").value= inputText;
-      });
+
+  let string= document.getElementById("inputMessage").value.toUpperCase();
+  let offset= parseInt(document.getElementById("offset").value);
+  let decodeText=cipher.decode(string,offset);
+    
+    document.getElementById("outputMessage").value=decodeText ;
+});
+
+/// BOTON LIMPIAR
+document.getElementById("reset").addEventListener("click", () => {
+
+  document.getElementById("inputMessage").value="";
+  document.getElementById("offset").value="";
+  document.getElementById("outputMessage").value="";
+});
